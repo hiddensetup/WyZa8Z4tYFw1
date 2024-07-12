969ebb2e702bd6caf371213ede467edeae7c12cc
hiddensetup <franka@dublin.com>
2024-07-12 13:35:01 -0300

    test

--- apps/waweb/functions.php
+++ apps/waweb/functions.php
@@ -2,7 +2,7 @@
 define('SB_WAWEB', 'Go');
 
 // Define a list of blacklisted numbers
-$blacklist = ['5491165900208', '5491126415491', '34660172262', '50761519259', '5491140908465','525574957250']; // Example blacklisted numbers
+$blacklist = ['5491126415491', '34660172262', '50761519259', '5491140908465','525574957250']; // Example blacklisted numbers
 
 function sb_waweb_send_message($to, $message = '', $attachments = [])
 {
